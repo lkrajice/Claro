eb1f81e03f080d9eea47f4e74be63c5afc7c8c55
Filip Bartos <devbygames@gmail.com>
2018-03-23 21:58:24 +0100

    Added testing template, improved index function in view

--- parlament/views.py
+++ parlament/views.py
@@ -4,7 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.template import loader
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Surprise MotherFucker :)</h1>")
+    co_je_anime = "bullshit"
+    template = loader.get_template('index.html')
+    context = {
+        "co_je_anime" : co_je_anime    
+     }
+    return HttpResponse(template.render(context, request))
