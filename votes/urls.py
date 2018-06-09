9534b7a69c89d50ec90095520d58e6201c5f64fc
Lukas Krajicek <lkrajice@redhat.com>
2018-06-09 20:16:46 +0200

    Improve urls and items in side bar

--- votes/urls.py
+++ votes/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.overview),
-    url(r'nomination', views.nomination_overview),
+    url(r'overview', views.overview),
     url(r'class', views.class_overview),
-    url(r'election', views.election_overview)
+    url(r'', views.overview),
 ]
--- votes/views.py
+++ votes/views.py
@@ -1,25 +1,43 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import inspect
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.template import loader
 
 
-DEFAULT_CONTEXT = {'active_app': 'votes'}
+DEFAULT_CONTEXT = {'metadata_active_app': 'votes'}
+
+
+def get_default_context():
+    """
+    Return default context
+    """
+    currentframe = inspect.currentframe()
+    outerframe = inspect.getouterframes(currentframe, 1)
+
+    # get the name of function which called this one
+    caller_name = outerframe[1][3]
+
+    context = DEFAULT_CONTEXT
+    context['metadata_active_view'] = caller_name
+
+    return context
 
 
 def overview(request):
     """
     Shown nomination or election overview based on which procces is active
     """
-    return nomination_overview(request)
+    return nomination_overview(request, get_default_context())
 
 
 ### VIEWS #########################################################################################
 
-def nomination_overview(request):
+def nomination_overview(request, default_context):
     """
     Shows nominated students for every class
 
@@ -38,20 +56,35 @@ def nomination_overview(request):
     """
     template = loader.get_template('nomination_overview.html')
     context = {
-        'classes': {
-            'V*': {
-                '2': ['V2A', 'V2B', 'V2C', 'V2D'],
-                '3': ['V3A', 'V3B', 'V3C', 'V3D'],
-                '4': ['V4A', 'V4B', 'V4C', 'V4D'],
-            },
-            'S*': {
-                '2': ['S2A', 'S2B', 'S2C'],
-                '3': ['S3A', 'S3B', 'S3C'],
-                '4': ['S4A', 'S4B', 'S4C'],
-            },
-        },
+        'key': 'value'
     }
-    context.update(DEFAULT_CONTEXT)
+    context.update(default_context)
+    return HttpResponse(template.render(context, request))
+
+
+def election_overview(request, default_context):
+    """
+    Shows candidates and progress of election
+
+    SHOWS:
+        All candidated
+            name
+            picture
+            number of points
+
+    ACTIONS:
+        Trigger dialog which shown detailed info about a candidate
+        Vote for an candidate which is showed in triggered dialog
+
+    PROCCESS:
+        Load all candidate
+
+    """
+    template = loader.get_template('election_everview.html')
+    context = {
+        'key': 'value'
+    }
+    context.update(default_context)
     return HttpResponse(template.render(context, request))
 
 
@@ -74,33 +107,20 @@ def class_overview(request):
         Load all students from given class willing to candidate
 
     """
-    template = loader.get_template('class_everview.html')
+    template = loader.get_template('class_overview.html')
     context = {
-        'key': 'value'
+        'classes': {
+            'V*': {
+                '2': ['V2A', 'V2B', 'V2C', 'V2D'],
+                '3': ['V3A', 'V3B', 'V3C', 'V3D'],
+                '4': ['V4A', 'V4B', 'V4C', 'V4D'],
+            },
+            'S*': {
+                '2': ['S2A', 'S2B', 'S2C'],
+                '3': ['S3A', 'S3B', 'S3C'],
+                '4': ['S4A', 'S4B', 'S4C'],
+            },
+        },
     }
-    return HttpResponse(template.render(context, request))
-
-
-def election_overview(request):
-    """
-    Shows candidates and progress of election
-
-    SHOWS:
-        All candidated
-            name
-            picture
-            number of points
-
-    ACTIONS:
-        Trigger dialog which shown detailed info about a candidate
-        Vote for an candidate which is showed in triggered dialog
-
-    PROCCESS:
-        Load all candidate
-
-    """
-    template = loader.get_template('election_everview.html')
-    context = {
-        'key': 'value'
-    }
+    context.update(get_default_context())
     return HttpResponse(template.render(context, request))
